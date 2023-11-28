3c4fa23a7da17e16bf0920fc552f6c62409ff815
tabib-e-alahi <etabib.alahi@gmail.com>
2023-11-28 15:08:09 +0600

    loaded all camps data get api

--- index.js
+++ index.js
@@ -1,6 +1,60 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
+
+//middlewares
+app.use(cors())
+app.use(express.json())
+
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.o8c7bsg.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+    const campsCollection = client.db("mediCampsDb").collection("camps");
+
+    // load all camps data from mongodb database 
+    app.get('/camps', async(req,res) =>{
+      const result = await campsCollection.find().toArray();
+      res.send(result);
+    })
+
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+
+
+
 
 
 
